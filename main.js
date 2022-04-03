4e4542cc0f65d8be30ad256d36215517531e6aab
ditam <ditam@users.noreply.github.com>
2022-04-03 12:37:38 +0200

    add basic flooding mechanic

--- main.js
+++ main.js
@@ -18,10 +18,79 @@ const levelLayouts = [
   ]
 ];
 
-const tileCode2Type = {
-  0: 'grass',
-  1: 'water'
-};
+// this var is used both as a type checking tool,
+// as well as an index->type mapping for the level layouts
+// (This means order is important!)
+const tileTypes = ['grass', 'water'];
+
+function getNeighbours(i, j) {
+  console.assert(i >= 0 && j >= 0);
+
+  const neighbours = [];
+  if (i-1 >= 0) {
+    // tile above
+    neighbours.push(map[i-1][j]);
+  }
+  if (j+1 < COL_COUNT) {
+    // tile to the right
+    neighbours.push(map[i][j+1]);
+  }
+  if (i+1 < ROW_COUNT) {
+    // tile below
+    neighbours.push(map[i+1][j]);
+  }
+  if (j-1 >= 0) {
+    // tile to the left
+    neighbours.push(map[i][j-1]);
+  }
+
+  return neighbours;
+}
+
+function floodNeighbours(i, j) {
+  // floods the neighbouring tiles, considers dams and applies decay
+  for (const tile of getNeighbours(i, j)) {
+    if (tile.type === 'grass') {
+      tile.type = 'water';
+      tile.updated = true;
+      tile.domNode.removeClass(tileTypes);
+      tile.domNode.addClass('water');
+    }
+  }
+}
+
+function updateMap() {
+  // updates the map types in-place by applying flooding rules
+
+  for (let ri = 0; ri < map.length; ri++) {
+    const row = map[ri];
+    for (let ci = 0; ci < row.length; ci++) {
+      const cell = row[ci];
+      console.assert(tileTypes.includes(cell.type));
+
+      if (cell.type === 'water' && !cell.updated) {
+        floodNeighbours(ri, ci);
+      }
+    }
+  }
+
+  // clear updated flags
+  for (let ri = 0; ri < map.length; ri++) {
+    const row = map[ri];
+    for (let ci = 0; ci < row.length; ci++) {
+      const cell = row[ci];
+      console.assert(tileTypes.includes(cell.type));
+      delete cell.updated;
+    }
+  }
+}
+
+function endTurn() {
+  console.log('ending turn...');
+
+  updateMap();
+  // TODO: apply worker effects and remove workers
+}
 
 function init() {
   for (let i=0; i<ROW_COUNT; i++) {
@@ -29,7 +98,7 @@ function init() {
     for (let j=0; j<COL_COUNT; j++) {
       const tileCode = levelLayouts[currentLevel][i][j];
       row.push({
-        type: tileCode2Type[tileCode]
+        type: tileTypes[tileCode]
       });
     }
     map.push(row);
@@ -40,7 +109,7 @@ function init() {
     const rowDiv = $('<div />').addClass('row');
     for (let ci = 0; ci < row.length; ci++) {
       const cell = row[ci];
-      console.assert(['grass', 'water'].includes(cell.type));
+      console.assert(tileTypes.includes(cell.type));
       const cellDiv = $('<div />').addClass('tile').addClass(cell.type);
       cellDiv.css({
         width: TILE_SIZE,
@@ -49,6 +118,8 @@ function init() {
       cellDiv.on('click', () => {
         console.log('click:', ri, ci, cell);
       });
+      // we add a reference to the dom node
+      cell.domNode = cellDiv;
       cellDiv.appendTo(rowDiv);
     }
     rowDiv.appendTo(container);
@@ -59,4 +130,8 @@ $(document).ready(function() {
   console.log('init');
   container = $('#map-container');
   init();
+
+  $('#end-turn-button').on('click', () => {
+    endTurn();
+  });
 });
